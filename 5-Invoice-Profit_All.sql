25dce42b6f87879bd9d48aaf6cee61a96b920ca2
Katy <katayoun.esmaeili@gmail.com>
2023-08-01 19:34:11 -0600

    Update 5-Invoice-Profit_All.sql

--- 5-Invoice-Profit_All.sql
+++ 5-Invoice-Profit_All.sql
@@ -1,5 +1,3 @@
-SET @startdate = '2020-03-21 00:00:00' , @finishdate = '2021-03-21 00:00:00' ,@username = 'owjinia';
-use vtigercrm600;
 
   select floor(sum(subquery.productprice)) as productprice,floor(sum(subquery.productprice-subquery.totalprice)) as profitprice
  
